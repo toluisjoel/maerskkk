e0ff7d212147505304194d125c3bdd87e7a56aec
Tolu Joel <tolu@Tolus-MacBook-Pro.local>
2022-09-03 05:58:32 +0100

    August Dump

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+/* When the user scrolls down, hide the navbar.
+When the user scrolls up, show the navbar */
+
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+hero = document.getElementById("hero");
+
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
+
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
+
   } else {
-    document.getElementsByClassName("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-100px";
   }
+
   prevScrollpos = currentScrollPos;
 }
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+
+//   if (prevScrollpos >= currentScrollPos || currentScrollPos <= document.getElementById("navbar").offsetHeight) {
+//     document.getElementById("navbar").style.top = "0";
+//     document.getElementById("navbar").style.background = "#2b2b2b";
+//     document.getElementById("totop").style.bottom = "-120px";
+
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//     document.getElementById("navbar").style.background = "transparent";
+//     document.getElementById("totop").style.bottom = "0px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
